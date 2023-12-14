53e6887daf14c8b50de9d7185fc06e43efa523ea
Abela <abelzerabruk@gmail.com>
2023-12-14 16:22:50 +0300

    packaged using nuget

---
+++ arifpaynet/arifpaynet.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Net;
+namespace arifpaynet;
+
+public class ArifPay
+{
+    private string API_key;
+
+
+    public ArifPay(string API_key)
+    {
+        this.API_key = API_key;
+    }
+
+    public async Task<JObject> Make_payment(JObject payment_info)
+    {
+        List<string> requiredFields = new List<string> { "cancelUrl", "successUrl", "errorUrl", "notifyUrl", "paymentMethods", "items", "beneficiaries" };
+        List<string> missingFields = new List<string>();
+
+        foreach (string url in requiredFields)
+        {
+            if (!payment_info.ContainsKey(url))
+            {
+                missingFields.Add(url);
+            }
+        }
+
+        if (missingFields.Count > 0)
+        {
+            JObject missingFieldsObj = new JObject();
+
+            foreach (string field in missingFields)
+            {
+                missingFieldsObj.Add(field, field + " is a required field please enter this field");
+            }
+
+            return missingFieldsObj;
+        }
+        else
+        {
+           // payment_info.Add("nonce", Guid.NewGuid().ToString());
+           // payment_info.Add("expireDate", this.expireDate);
+
+            if (missingFields.Count == 0)
+            {
+                string url = "https://gateway.arifpay.org/api/sandbox/checkout/session";
+
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.DefaultRequestHeaders.Add("x-arifpay-key", this.API_key);
+
+                        StringContent content = new StringContent(payment_info.ToString(), Encoding.UTF8, "application/json");
+
+                        HttpResponseMessage response = await client.PostAsync(url, content);
+                        if ((int)response.StatusCode != (int)HttpStatusCode.OK)
+                        {
+                            
+                            JObject error = new JObject();
+                            error.Add("status", (int)response.StatusCode);
+                            error.Add("message", response.ReasonPhrase);
+                            return error;
+                        }
+                        string result = await response.Content.ReadAsStringAsync();
+                        return JObject.Parse(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                   Console.WriteLine(e);
+                }
+            }
+             JObject r = new JObject();
+             r.Add("status", "fail");
+             r.Add("message", "error");
+             return r;
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,80 @@
+# ArifPay
+
+ArifPay is a C# library for making payments using ArifPay.
+
+## Installation
+
+This package is available on NuGet. You can install it using the following command:
+
+```bash
+dotnet add package arifpaynet
+
+Usage
+
+Here’s a basic example of how to use the ArifPay library:
+
+using System;
+using Newtonsoft.Json.Linq;
+using arifpaynet;
+
+public class Program
+{
+    public static void Main()
+    {
+        ArifPay arifPay = new ArifPay("your_api_key_here");
+
+        //  create your request body using this format
+        var requestbody = new Dictionary<string, object>()
+        {
+            {"items", new Dictionary<string, object>[] {new Dictionary<string,object>()
+                {
+                    {"name", "abel"},
+                    {"quantity", 2},
+                    {"price", 10},
+                    {"description", "some description"},
+                    {"image", "https://rb.gy/od65py"},
+                }
+                }
+            },
+            {"beneficiaries",new Dictionary<string, object>[] {new Dictionary<string,object>()
+                {
+                    {"accountNumber", "164910326"},
+                    {"bank", "ABYSETAA"},
+                    {"amount", 20},
+                }
+            }
+            },
+            {"cancelUrl","http://example.com"},
+            {"errorUrl","http://example.com"},
+            {"notifyUrl","http://example.com"},
+            {"successUrl","http://example.com"},
+            {"paymentMethods",new string[] { "Telebirr" }},
+            {"lang","EN"},
+            {"nonce",Guid.NewGuid().ToString()},
+            {"expireDate","2025-12-02T03:45:12"}
+        };
+
+        JObject jsonObject = JObject.FromObject(requestbody);
+        // finally make use of the Make_payment method
+        JObject result = arifPay.Make_payment(jsonObject).Result;
+        // you can see the result json
+        Console.WriteLine(result);
+
+
+
+
+
+
+
+        // Add payment information to paymentInfo...
+        paymentInfo.Add("cancelUrl", "<your-cancel-url>");
+        paymentInfo.Add("successUrl", "<your-success-url>");
+        paymentInfo.Add("errorUrl", "<your-error-url>");
+        paymentInfo.Add("notifyUrl", "<your-notify-url>");
+        paymentInfo.Add("paymentMethods", "<your-payment-methods>");
+        paymentInfo.Add("items", "<your-items>");
+        paymentInfo.Add("beneficiaries", "<your-beneficiaries>");
+        // finally make use of the Make_payment method
+        JObject result = arifPay.Make_payment(paymentInfo).Result;
+    }
+}
